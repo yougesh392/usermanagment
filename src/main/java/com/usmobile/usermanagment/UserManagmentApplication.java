952092156857a863f8b4c4f9edaa1035f070f17b
yougesh392 <ypinjani@gmail.com>
2024-05-16 10:31:14 -0400

    added test data

---
+++ src/main/java/com/usmobile/usermanagment/UserManagmentApplication.java
@@ -0,0 +1,37 @@
+package com.usmobile.usermanagment;
+
+import com.usmobile.usermanagment.DAO.UserDAO;
+import com.usmobile.usermanagment.repository.UserRepository;
+import com.usmobile.usermanagment.utils.EncryptionUtil;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@SpringBootApplication
+@EnableMongoAuditing
+public class UserManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagmentApplication.class, args);
+	}
+
+	/*
+	* loads the test data into the database
+	* */
+	@Bean
+	CommandLineRunner runner(UserRepository userRepository) {
+		return args -> {
+			for (int i = 1; i <= 10; i++) {
+				UserDAO user = new UserDAO();
+				user.setFirstName("Test" + i);
+				user.setLastName("User");
+				user.setEmail("test" + i + "@example.com");
+				user.setPassword(EncryptionUtil.encode("password"));
+				user.setPhoneNumber("123456789" + i);
+				userRepository.save(user);
+			}
+		};
+	}
+}
--- src/main/java/com/usmobile/usermanagment/controller/ErrorController.java
+++ src/main/java/com/usmobile/usermanagment/controller/ErrorController.java
@@ -5,6 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
 
+
+/*
+* Global exception controller
+*
+* */
 public class ErrorController {
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<ApiError> handleResponseStatusException(ResponseStatusException ex) {
